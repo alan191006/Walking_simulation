41a1784f7626075527a5de17615c0fbe4766854f
Vasa123456 <liekbigmoney@gmail.com>
2022-09-05 14:01:37 +1000

    Book working :)
    physics disabled on pause

--- Levels/Main/L_Main.gd
+++ Levels/Main/L_Main.gd
@@ -26,12 +26,6 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit() # Quits the game
 	
-	if event.is_action_pressed("Book"):
-		match Input.get_mouse_mode():
-			Input.MOUSE_MODE_CAPTURED:
-				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-			Input.MOUSE_MODE_VISIBLE:
-				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
 # Capture mouse if clicked on the game, needed for HTML5
